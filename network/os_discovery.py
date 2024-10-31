5f29a8be32b57b1bea300c3ff51dff68f15f8756
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-10-31 16:23:13 +0000

    add: colors

--- network/os_discovery.py
+++ network/os_discovery.py
@@ -8,7 +8,6 @@ import subprocess
 
 def os_discovery(Host):
 
-    # More secure than check_call
     service = f"nmap {Host} -sV -version-intensity 8 -T4 --host-timeout 120"
     oS = f"nmap {Host} -O -T4 --host-timeout 120"
     output = subprocess.check_output(service, shell=True).decode('utf-8')
